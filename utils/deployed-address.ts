1c8899d27214ffe019addc63b98c079a87f067be
phanthienhung1994 <phanthienhung1994@gmail.com>
2024-08-23 16:51:17 +0000

    updataa

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0x1a104BB08Cdb346E3dD9a7cF79C0f92Ea1A50751'
+export const ProxyAdmin = '0x7B20048Bf3C7A1c810455a2d816e0216b9F763B0'
+export const SWTRImplementation = '0x326C6238d67879d812C67937d27A01dC47618d4D'
     
